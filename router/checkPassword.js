4f5bab3c0123b8c54f310697141f41e0f84ddd40
mysper <anthony.a3203@gmail.com>
2021-01-09 02:08:00 +0800

    stored password on database

--- router/checkPassword.js
+++ router/checkPassword.js
@@ -1,8 +1,17 @@
-function check(req, res, next) {
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const pwd = mongoose.model(
+    'Info',
+    new Schema({ password: String }),
+    'infomation'
+);
+
+async function check(req, res, next) {
     const { password } = req.body;
-    if (password == "!@#$%^&*()_+") next();
+    const pd = await pwd.find();
+    if (password == pd.password) next();
     else return res.status(400).json({ msg: 'the post is not allow' });
 }
-
 
 module.exports = check;
