2cbd5478f12c815d84c8540d61ec55664d662671
mysper <anthony.a3203@gmail.com>
2021-01-09 02:23:54 +0800

    current the database id

--- router/checkPassword.js
+++ router/checkPassword.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const pwd = mongoose.model(
-    'Info',
+    'infomation',
     new Schema({ password: String }),
     'infomation'
 );
 
 async function check(req, res, next) {
     const { password } = req.body;
-    const pd = await pwd.find();
+    const pd = await pwd.findById("5ff8947e455d649be40a9ffc");
+    console.log(pd);
     if (password == pd.password) next();
     else return res.status(400).json({ msg: 'the post is not allow' });
 }
